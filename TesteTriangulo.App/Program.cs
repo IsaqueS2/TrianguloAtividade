c47186cc615f7e057d5a871be9bb1dd78a6a230d
IsaqueS2 <isaq2@DESKTOP-4MDGU58>
2023-04-06 13:50:06 -0300

    Add project files.

---
+++ TesteTriangulo.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TesteTriangulo.App;
+
+
+namespace TesteTriangulo.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Digite os valores dos lados do triangulo:");
+            Console.Write("Lado 1:");
+            triangulo.Lado1 = double.Parse(Console.ReadLine());
+            Console.Write("Lado 2:");
+            triangulo.Lado2 = double.Parse(Console.ReadLine());
+            Console.Write("Lado 3:");
+            triangulo.Lado3 = double.Parse(Console.ReadLine());
+
+
+            triangulo.DefinirTipo();
+        }
+    }
+}
+
---
+++ TesteTriangulo.App/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTriangulo.App
+{
+    public class Triangulo
+    {
+        public double Lado1;
+        public double Lado2;
+        public double Lado3;
+            
+        public void DefinirTipo()
+        {
+            if (Lado1 < Lado2 + Lado3 && Lado2 < Lado1 + Lado3 && Lado3 < Lado1 + Lado2)
+            {
+                if (Lado1 == Lado2 && Lado1 == Lado3)
+                {
+                    Console.WriteLine("Este triangulo é equilátero");
+                }
+                else if (Lado1 != Lado2 && Lado1 != Lado3 && Lado2 != Lado3)
+                {
+                    Console.WriteLine("Este triangulo é escaleno");
+                }
+                else
+                {
+                    Console.WriteLine("Este triangulo é um Isósceles");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é um Triangulo");
+            }
+        }
+    }
+}
